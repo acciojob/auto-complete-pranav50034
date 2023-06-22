5215afa562cce40c1af78edd2be38d722c976c22
Pranav Pokale <pranavpokale50034@gmail.com>
2023-06-22 14:23:41 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,48 @@
 
-import React from "react";
+import React, {useState, useEffect} from "react";
 import './../styles/App.css';
+import Li from "./Li";
 
 const App = () => {
+
+  const fruits = [
+     "apple",
+     "banana",
+     "cherry",
+     "date",
+     "elderberry",
+     "fig",
+  ];
+
+  const [filter, setFilter] = useState("");
+
+  const [search, setSearch] = useState("")
+
+  useEffect(()=>{
+    setFilter(fruits.filter(fruit=> fruit.toLowerCase().includes(search.toLowerCase())
+    ))
+  },[search])
+  
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Search item</h1>
+        <input onChange={(e)=>setSearch(e.target.value)} type="text"/>
+        <ul>
+            {
+              !search &&
+              fruits.map((fruit, index)=>{
+                return <Li key={index} fruit={fruit} />
+              })
+            }
+            {
+              search &&
+              filter.map((fruit, index)=>{
+                return <Li key={index} fruit={fruit} />
+              })
+            }
+        </ul>
     </div>
   )
 }
---
+++ src/components/Li.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Li = ({key, fruit})=>{
+    return (
+        <li key={key}>{fruit}</li>
+    )
+}
+
+export default Li
